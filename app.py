0b1e60c5b4936a9aedab9f27f985d178b6f122e5
Hiya Sanghvi <169714000+hiyasanghvi@users.noreply.github.com>
2025-12-14 10:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import pydeck as pdk
 from utils import (
     create_chatroom, send_message,
     get_chatroom_messages, search_messages,DB_PATH,
-    list_user_chats
+    list_user_chats,  create_private_chatroom_if_not_exists 
 )
 # =======================================================
 # PAGE CONFIG + CSS
@@ -808,3 +808,4 @@ def main():
     elif nav == "Feed": feed_page()    
 if __name__ == "__main__":
     main()
+
